25ba7c2f0ff73a7ca9ef7efc34a1b03278e08017
YASHRAUTELA <yashrautela1@gmail.com>
2018-04-18 18:41:48 +0530

    18Apr2018 2nd attempt

--- StudentController.php
+++ StudentController.php
@@ -6,9 +6,31 @@ use App\Student;
 use Illuminate\Http\Request;
 use DateTime;
 use App\Course;
+use App\User;
+use DB;
 
 class StudentController extends Controller
 {
+
+    /*
+    *Rendering student data to the admin Panel
+    */
+    public function getStudent(){
+        /*$student_data=DB::table('users')->whereIn('id',function($query){
+            $query->select('user_id')->from('students');
+            })
+        ->get();*/
+        $student_data=User::where('role_id','=',3)->get();
+
+        if($student_data->count()){
+            return view('admin.students')->with('data',$student_data);    
+        }
+        else{
+            return view('admin.noStudent');
+        }
+    }
+
+
     /**
      * Display a listing of the resource.
      *
@@ -75,10 +97,10 @@ class StudentController extends Controller
         $student_data->mother_name=$request->mother;
         $student_data->save();
         if($student_data){
-            return redirect()->back()->with("success","Record saved successfully");;    
+            return redirect()->back()->with("success","Record saved successfully");    
         }
         else{
-            return redirect()->back()->with("error","Anonymous error occurred,Please try again");;
+            return redirect()->back()->with("error","Anonymous error occurred,Please try again");
         }
         
 
@@ -101,9 +123,38 @@ class StudentController extends Controller
      * @param  \App\Student  $student
      * @return \Illuminate\Http\Response
      */
-    public function edit(Student $student)
+    public function edit($id)
     {
-        //
+        /*select c.city_name,st.state_name,cr.course,s.* from students s INNER JOIN users u on u.id=s.user_id INNER JOIN cities c on c.id=s.city_id INNER JOIN states st on st.id=s.state_id INNER JOIN courses cr on cr.id= s.course_id WHERE s.user_id=19*/
+
+        $student_data=DB::table('students as s')
+                        ->join('users as u','u.id','=','s.user_id')
+                        ->join('courses as c','c.id','=','s.course_id')
+                        ->join('states as st','st.id','=','s.state_id')
+                        ->join('cities as ct','ct.id','=','s.city_id')
+                        ->where('s.user_id','=',$id)
+                        ->select('u.id','u.name','u.email','u.image_title','u.dob','ct.city_name','st.state_name','c.course','s.address','s.city_id','s.state_id','s.contact','s.course_id','s.registration_date','s.session','s.pin','s.father_name','s.mother_name')
+                        ->get();
+
+        
+        /*echo "<pre>";
+        print_r($student_data[0]);
+        echo "<br>";
+        exit;*/
+        if($student_data->count()){
+            return view('admin.updateStudent')->with('data',$student_data[0]);
+
+        }
+        else{
+            $user_data=User::find($id);
+            print_r($user_data->name);
+            echo "<br>hi";
+        }
+        exit;
+
+
+        /*$student_data=User::find($id);
+        return view('admin.updateStudent')->with('data',$student_data);*/
     }
 
     /**
@@ -113,9 +164,25 @@ class StudentController extends Controller
      * @param  \App\Student  $student
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Student $student)
+    public function update(Request $request)
     {
-        //
+        $student_data=$request->validate([
+            'id'=>'required',
+            'name'=>'required',
+            'email'=>'required|email',
+            'date'=>'required|date'
+            ]);
+
+        $student=User::find($request->id);
+        $student->name=$request->name;
+        $student->email=$request->email;
+        $student->dob=$request->date;
+        if($student->save()){
+            return redirect()->route('smsStudent')->with("update_success","Record updated successfully");
+        }
+        else{
+            return redirect()->back()->with("update_failure","Record not updated, Please try again");
+        }
     }
 
     /**
